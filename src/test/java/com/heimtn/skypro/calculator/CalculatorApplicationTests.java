8223886fec5349715cee81d0223c6dd9c54d0fd5
ZaretskiyM <zaretwork@gmail.com>
2023-04-24 22:20:07 +0300

    HomeWork Spring + Maven

---
+++ src/test/java/com/heimtn/skypro/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heimtn.skypro.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
